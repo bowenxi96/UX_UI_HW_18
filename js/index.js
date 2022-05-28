5968545e4efabec63f4c7a2ced7944d9cb419bf1
bowenxi <95603540+bowenxi96@users.noreply.github.com>
2022-05-28 00:15:01 -0400

    final added scroll up

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 900){
+            $('#topBtn').fadeIn();
+        } else{
+
+            $('#topBtn').fadeOut();
+
+
+        }
+    });
+
+$("#topBtn").click(function(){
+
+    $('html ,body').animate({scrollTop : 0},800)
+})
+
+});
